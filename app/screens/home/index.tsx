c0cb3ead2905a434e5dedc8e7d8ab0a460ef6088
Tuan Anh Le <leanhtuanptithcm@gmail.com>
2021-10-13 20:16:33 +0700

    feat: change param type

--- app/screens/home/index.tsx
+++ app/screens/home/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import { View } from 'react-native';
+import type { ScreenComponent } from '@navigation/utils';
+import { registerScreen } from '@navigation/utils';
 
-const HomeScreen = () => {
+const Name = 'HomeScreen';
+
+export type HomeScreenParam = {
+  [Name]: Record<string, unknown>;
+};
+
+const HomeScreen: ScreenComponent<HomeScreenParam, 'HomeScreen'> = () => {
   return <View />;
 };
 
-export default HomeScreen;
+export default registerScreen<HomeScreenParam, 'HomeScreen'>(
+  'HomeScreen',
+  HomeScreen,
+);
