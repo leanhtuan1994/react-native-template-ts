9547b1d1d0ae7a20eae169a1c6824b6e9417829d
Tuan Anh Le <tuanla@galaxy.one>
2021-10-18 22:07:34 +0700

    feat: update docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,29 +1,29 @@
 module.exports = {
-  title: "React Native Template TS",
+  title: 'React Native Template TS',
   tagline:
-    "Clean and minimalist React Native template for a quick start with TypeScript 🚀",
-  url: "https://leanhtuan1994.github.io",
-  baseUrl: "/react-native-template-ts/",
-  onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
-  organizationName: "leanhtuan1994",
-  projectName: "react-native-template-ts",
-  favicon: "img/favicon.ico",
+    'Clean and minimalist React Native template for a quick start with TypeScript 🚀',
+  url: 'https://leanhtuan1994.github.io',
+  baseUrl: '/react-native-template-ts/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  organizationName: 'leanhtuan1994',
+  projectName: 'react-native-template-ts',
+  favicon: 'img/favicon.ico',
   themeConfig: {
-    image: "img/icon.png",
+    image: 'img/icon.png',
     navbar: {
-      title: "React Native",
+      title: 'React Native Template TS',
       hideOnScroll: false,
       items: [
         {
-          href: "https://github.com/leanhtuan1994/react-native-template-ts",
-          label: "GitHub",
-          position: "right",
+          href: 'https://github.com/leanhtuan1994/react-native-template-ts',
+          label: 'GitHub',
+          position: 'right',
         },
       ],
     },
     prism: {
-      theme: require("prism-react-renderer/themes/dracula"),
+      theme: require('prism-react-renderer/themes/dracula'),
     },
     footer: {
       copyright: `Copyright © ${new Date().getFullYear()} <a rel="noreferrer" href="https://github.com/leanhtuan1994" target="_blank">Tuan Anh Le</a>. Built with Docusaurus.`,
@@ -31,16 +31,16 @@ module.exports = {
   },
   presets: [
     [
-      "@docusaurus/preset-classic",
+      '@docusaurus/preset-classic',
       {
         docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-          routeBasePath: "/",
+          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: '/',
           editUrl:
-            "https://github.com/leanhtuan1994/react-native-template-ts/edit/website/",
+            'https://github.com/leanhtuan1994/react-native-template-ts/edit/website/',
         },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       },
     ],
